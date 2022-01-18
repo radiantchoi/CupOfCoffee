736850ce9862c19d9f7a745dd748ea4f60f0ae5f
Gordon Choi <thefirate@gmail.com>
2022-01-18 17:01:33 +0900

    Attach view model to view controller

--- CupOfCoffee/Controllers/OrdersTableViewController.swift
+++ CupOfCoffee/Controllers/OrdersTableViewController.swift
@@ -9,6 +9,9 @@ import Foundation
 import UIKit
 
 class OrdersTableViewController: UITableViewController {
+    
+    private var orderListViewModel = OrderListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         populateOrders()
@@ -26,10 +29,31 @@ extension OrdersTableViewController {
         WebService().load(resource: resource) { result in
             switch result {
             case .success(let orders):
-                print(orders)
+                self.orderListViewModel.ordersViewModel = orders.map(OrderViewModel.init)
+                self.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+}
+
+extension OrdersTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.orderListViewModel.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let viewModel = self.orderListViewModel.orderViewModel(at: indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrdersTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = viewModel.coffee
+        cell.detailTextLabel?.text = viewModel.size
+        
+        return cell
     }
 }
